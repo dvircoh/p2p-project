0251a2c8a686b99de58da8aae4b5ac9e5edb48fd
Dvir Cohen <dvir1.cohen@intel.com>
2023-01-21 22:45:01 +0200

    meybe fix?

--- peer_test_github.py
+++ peer_test_github.py
@@ -24,6 +24,7 @@ async def main():
         # Check requst files list from tracker
         send_files_list_message = send_files_list_handler()
         files_list_result = await send_and_recv_tracker(tracker_ip, send_files_list_message)
+        print(str(files_list_result))
         assert files_list_result == results_for_assert[0], "request files list failed"
         print("Test: request files list succeeded")
 
@@ -33,9 +34,11 @@ async def main():
         files_list_result = await send_and_recv_tracker(tracker_ip, send_files_list_message)
         print(str(files_list_result))
         assert result and (files_list_result == results_for_assert[1]), "add file 'hello_world' to tracker failed"
+
         message = add_file_handler("exemple1")
         result = await send_to_tracker(tracker_ip, message)
         files_list_result = await send_and_recv_tracker(tracker_ip, send_files_list_message)
+        print(str(files_list_result))
         assert result and (files_list_result == results_for_assert[2]), "add file 'exemple1' to tracker failed"
         print("Test: add files to tracker succeeded")
 
